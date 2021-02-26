71f3833f0911ec998f46df35d1630f81cd1c1e94
Ken Yeh <yehitsken@gmail.com>
2021-02-26 09:47:36 -0800

    api keys updated

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "ade90a74d2d25acdb8dcdd2e0fb7715a"
+weather_api_key = "YOUR KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyDy31OiSicDudPoMBxgtT4od5vpcMLSTzw"
+g_key = "YOUR KEY HERE"
